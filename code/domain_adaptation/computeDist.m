dd01698a8fdc5f1c833b159391e1d969f9c4b022
Andi Han <68631477+andyjm3@users.noreply.github.com>
2022-01-29 07:44:27 +1100

    Delete code directory

--- code/domain_adaptation/computeDist.m
+++
@@ -1,9 +0,0 @@
-function myDist = computeDist(X, Y)
-    m = size(X, 3);
-    n = size(Y, 3);
-    for ii = 1 : m
-        for jj = 1 : n
-            myDist(ii, jj) = norm(X(:,:,ii) - Y(:,:,jj), 'fro')^2 ;
-        end
-    end
-end
--- code/domain_adaptation/computeDistriemannian.m
+++
@@ -1,9 +0,0 @@
-function myDist = computeDistriemannian(X, Y)
-    m = size(X, 3);
-    n = size(Y, 3);
-    for ii = 1 : m
-        for jj = 1 : n
-            myDist(ii, jj) = RiemannianDist(X(:,:,ii), Y(:,:,jj), 2)^2;
-        end
-    end
-end
